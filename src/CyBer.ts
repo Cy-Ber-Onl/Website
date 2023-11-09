0511eb02ff7075c0b9d75fa4984f0fcd2e09cd85
Adam Post <adamrpostjr@gmail.com>
2023-11-08 22:14:31 -0500

    Coming Soon Page

---
+++ src/CyBer.ts
@@ -0,0 +1,101 @@
+// You can also use the generator at https://skeleton.dev/docs/generator to create these values for you
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+export const CyBer: CustomThemeConfig = {
+	name: 'CyBer',
+	properties: {
+		// =~= Theme Properties =~=
+		"--theme-font-family-base": `system-ui`,
+		"--theme-font-family-heading": `system-ui`,
+		"--theme-font-color-base": "0 0 0",
+		"--theme-font-color-dark": "255 255 255",
+		"--theme-rounded-base": "9999px",
+		"--theme-rounded-container": "8px",
+		"--theme-border-base": "1px",
+		// =~= Theme On-X Colors =~=
+		"--on-primary": "255 255 255",
+		"--on-secondary": "0 0 0",
+		"--on-tertiary": "0 0 0",
+		"--on-success": "0 0 0",
+		"--on-warning": "0 0 0",
+		"--on-error": "255 255 255",
+		"--on-surface": "255 255 255",
+		// =~= Theme Colors  =~=
+		// primary | #2e6418 
+		"--color-primary-50": "224 232 220", // #e0e8dc
+		"--color-primary-100": "213 224 209", // #d5e0d1
+		"--color-primary-200": "203 216 197", // #cbd8c5
+		"--color-primary-300": "171 193 163", // #abc1a3
+		"--color-primary-400": "109 147 93", // #6d935d
+		"--color-primary-500": "46 100 24", // #2e6418
+		"--color-primary-600": "41 90 22", // #295a16
+		"--color-primary-700": "35 75 18", // #234b12
+		"--color-primary-800": "28 60 14", // #1c3c0e
+		"--color-primary-900": "23 49 12", // #17310c
+		// secondary | #5ad926 
+		"--color-secondary-50": "230 249 222", // #e6f9de
+		"--color-secondary-100": "222 247 212", // #def7d4
+		"--color-secondary-200": "214 246 201", // #d6f6c9
+		"--color-secondary-300": "189 240 168", // #bdf0a8
+		"--color-secondary-400": "140 228 103", // #8ce467
+		"--color-secondary-500": "90 217 38", // #5ad926
+		"--color-secondary-600": "81 195 34", // #51c322
+		"--color-secondary-700": "68 163 29", // #44a31d
+		"--color-secondary-800": "54 130 23", // #368217
+		"--color-secondary-900": "44 106 19", // #2c6a13
+		// tertiary | #7D8CA3 
+		"--color-tertiary-50": "236 238 241", // #eceef1
+		"--color-tertiary-100": "229 232 237", // #e5e8ed
+		"--color-tertiary-200": "223 226 232", // #dfe2e8
+		"--color-tertiary-300": "203 209 218", // #cbd1da
+		"--color-tertiary-400": "164 175 191", // #a4afbf
+		"--color-tertiary-500": "125 140 163", // #7D8CA3
+		"--color-tertiary-600": "113 126 147", // #717e93
+		"--color-tertiary-700": "94 105 122", // #5e697a
+		"--color-tertiary-800": "75 84 98", // #4b5462
+		"--color-tertiary-900": "61 69 80", // #3d4550
+		// success | #79a9d1 
+		"--color-success-50": "235 242 248", // #ebf2f8
+		"--color-success-100": "228 238 246", // #e4eef6
+		"--color-success-200": "222 234 244", // #deeaf4
+		"--color-success-300": "201 221 237", // #c9dded
+		"--color-success-400": "161 195 223", // #a1c3df
+		"--color-success-500": "121 169 209", // #79a9d1
+		"--color-success-600": "109 152 188", // #6d98bc
+		"--color-success-700": "91 127 157", // #5b7f9d
+		"--color-success-800": "73 101 125", // #49657d
+		"--color-success-900": "59 83 102", // #3b5366
+		// warning | #ece2d0 
+		"--color-warning-50": "252 251 248", // #fcfbf8
+		"--color-warning-100": "251 249 246", // #fbf9f6
+		"--color-warning-200": "250 248 243", // #faf8f3
+		"--color-warning-300": "247 243 236", // #f7f3ec
+		"--color-warning-400": "242 235 222", // #f2ebde
+		"--color-warning-500": "236 226 208", // #ece2d0
+		"--color-warning-600": "212 203 187", // #d4cbbb
+		"--color-warning-700": "177 170 156", // #b1aa9c
+		"--color-warning-800": "142 136 125", // #8e887d
+		"--color-warning-900": "116 111 102", // #746f66
+		// error | #850101 
+		"--color-error-50": "237 217 217", // #edd9d9
+		"--color-error-100": "231 204 204", // #e7cccc
+		"--color-error-200": "225 192 192", // #e1c0c0
+		"--color-error-300": "206 153 153", // #ce9999
+		"--color-error-400": "170 77 77", // #aa4d4d
+		"--color-error-500": "133 1 1", // #850101
+		"--color-error-600": "120 1 1", // #780101
+		"--color-error-700": "100 1 1", // #640101
+		"--color-error-800": "80 1 1", // #500101
+		"--color-error-900": "65 0 0", // #410000
+		// surface | #242424 
+		"--color-surface-50": "222 222 222", // #dedede
+		"--color-surface-100": "211 211 211", // #d3d3d3
+		"--color-surface-200": "200 200 200", // #c8c8c8
+		"--color-surface-300": "167 167 167", // #a7a7a7
+		"--color-surface-400": "102 102 102", // #666666
+		"--color-surface-500": "36 36 36", // #242424
+		"--color-surface-600": "32 32 32", // #202020
+		"--color-surface-700": "27 27 27", // #1b1b1b
+		"--color-surface-800": "22 22 22", // #161616
+		"--color-surface-900": "18 18 18", // #121212
+	}
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import {onMount} from 'svelte';
+	let cs = ''
+
+	onMount(()=>{
+		// type out "coming soon..." with a flashing ibeam 
+		let i = 0
+		let text = 'coming soon...'
+		let interval = setInterval(()=>{
+			if(i < text.length){
+				cs += text[i]
+				i++
+			}else{
+				clearInterval(interval)
+				cs += '<span class="ibeam">|</span>'
+			}
+		}, 100)
+	})
+
+</script>
+
+<div class="w-full h-full flex flex-col justify-center">
+	<div class="card w-2/3 h-2/3 mx-auto overflow-hidden">
+		<div class="w-full justify-end flex gap-2 pt-4 pb-2 px-4">
+			<span class="w-3 h-3 block variant-filled-success rounded-full" />
+			<span class="w-3 h-3 block variant-filled-warning rounded-full" />
+			<span class="w-3 h-3 block variant-filled-error rounded-full" />
+		</div>
+		<div class="bg-black w-full h-full rounded p-4">
+			{@html cs}
+		</div>
+	</div>
+</div>
+
+<style>
+	:global(.ibeam) {	
+		animation: blink 1s infinite;
+	}
+	@keyframes blink {
+		0% {
+			opacity: 0;
+		}
+		50% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
+</style>
